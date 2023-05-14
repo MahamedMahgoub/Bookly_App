eadc3d5572815eb0bb91cdd28fbcf3655f1c51ec
Mohamed Samy <m.samy.mahgoub97@gmail.com>
2023-05-14 04:51:48 +0200

    Add custom image to book details view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_book_details_appbar.dart';
@@ -7,11 +8,16 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
-        children: const [
-          CustomBookDetailsAppbar(),
+        children: [
+          const CustomBookDetailsAppbar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .15),
+            child: const CustomBookImage(),
+          ),
         ],
       ),
     );
--- lib/features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,4 +1,4 @@
-import 'package:bookly/features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
@@ -7,13 +7,13 @@ class FeaturedBooksListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
+      height: MediaQuery.of(context).size.height * .25,
       child: ListView.builder(
           itemCount: 10,
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) => const Padding(
                 padding: EdgeInsets.only(right: 8),
-                child: FeaturedListViewItem(),
+                child: CustomBookImage(),
               )),
     );
   }
