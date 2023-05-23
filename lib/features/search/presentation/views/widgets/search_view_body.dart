cbba953013ce4a1739618fa4d8d2b38b2a6aab7f
Mohamed Samy <m.samy.mahgoub97@gmail.com>
2023-05-23 02:28:07 +0200

    srach view & ui done

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utlis/styles.dart';
+import '../../../../home/presentation/views/widgets/best_seller_list_item.dart';
 import 'custom_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -8,12 +10,34 @@ class SearchViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 16),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: const [
           CustomSearchTextField(),
+          SizedBox(height: 24),
+          Text('Search Result', style: Styles.testStyle18),
+          Expanded(child: SearchResuleListView()),
         ],
       ),
+    );
+  }
+}
+
+class SearchResuleListView extends StatelessWidget {
+  const SearchResuleListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: BestSellerListViewItem(),
+        );
+      },
     );
   }
 }
